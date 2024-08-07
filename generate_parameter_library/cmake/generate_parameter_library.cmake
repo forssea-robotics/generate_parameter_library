9cb754117f39aef6e57cf1b9de8533f82296468a
AugusteBourgois <auguste@forssea-robotics.fr>
2024-08-07 10:04:31 +0200

    deprecate locally accessible generated header

--- generate_parameter_library/cmake/generate_parameter_library.cmake
+++ generate_parameter_library/cmake/generate_parameter_library.cmake
@@ -81,12 +81,25 @@ macro(generate_parameter_library LIB_NAME YAML_FILE)
     "Creating deprecated header file ${LOCAL_PARAM_HEADER_FILE}"
     VERBATIM
   )
+  # necessary so that #include <param_file.hpp> can be used in the local package (deprecated)
+  set(LOCAL_PARAM_HEADER_FILE ${CMAKE_CURRENT_BINARY_DIR}/include/${LIB_NAME}.hpp)
+  add_custom_command(
+      OUTPUT ${LOCAL_PARAM_HEADER_FILE}
+      COMMAND ${CMAKE_COMMAND} -E echo "#pragma warning(\"`#include <${LIB_NAME}>` is deprecated. Use `#include <${PROJECT_NAME}/${LIB_NAME}.hpp>` instead.\")" >> ${LOCAL_PARAM_HEADER_FILE}
+      COMMAND ${CMAKE_COMMAND} -E cat ${LOCAL_PARAM_HEADER_FILE} ${PARAM_HEADER_FILE} > ${LOCAL_PARAM_HEADER_FILE}.tmp
+      COMMAND ${CMAKE_COMMAND} -E copy ${LOCAL_PARAM_HEADER_FILE}.tmp ${LOCAL_PARAM_HEADER_FILE}
+      COMMAND ${CMAKE_COMMAND} -E remove ${LOCAL_PARAM_HEADER_FILE}.tmp
+      DEPENDS ${PARAM_HEADER_FILE}
+      COMMENT
+      "Creating deprecated header file ${LOCAL_PARAM_HEADER_FILE}"
+      VERBATIM
+  )
 
   # Create the library target
   add_library(${LIB_NAME} INTERFACE ${PARAM_HEADER_FILE} ${VALIDATE_HEADER} ${LOCAL_PARAM_HEADER_FILE})
   target_include_directories(${LIB_NAME} INTERFACE
-    $<BUILD_INTERFACE:${LIB_INCLUDE_DIR}>
-    $<INSTALL_INTERFACE:include/${PROJECT_NAME}/${LIB_NAME}>
+    $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/include>
+    $<INSTALL_INTERFACE:include>
   )
   set_target_properties(${LIB_NAME} PROPERTIES LINKER_LANGUAGE CXX)
   target_link_libraries(${LIB_NAME} INTERFACE
@@ -98,7 +111,9 @@ macro(generate_parameter_library LIB_NAME YAML_FILE)
     tcb_span::tcb_span
     tl_expected::tl_expected
   )
-  install(DIRECTORY ${LIB_INCLUDE_DIR} DESTINATION include/${PROJECT_NAME}/${LIB_NAME})
+  install(DIRECTORY ${LIB_INCLUDE_DIR} DESTINATION include)
+  ament_export_dependencies(fmt parameter_traits rclcpp rclcpp_lifecycle rsl tcb_span tl_expected)
+  set(_AMENT_CMAKE_EXPORT_DEPENDENCIES "${_AMENT_CMAKE_EXPORT_DEPENDENCIES}" PARENT_SCOPE)
 endfunction()
 
 
